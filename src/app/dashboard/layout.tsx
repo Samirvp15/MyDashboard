8cbb58203e2f7500e9649dca4ce76da97ae44eec
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-12-18 22:55:54 -0500

    add PageNotFound for Pokemon and Pages

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,16 +5,16 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
 
     return (
         <>
-        <div className="bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white">
-        <div className="flex">
-           
-            <Sidebar /> 
-            <div className=" p-2 w-full text-slate-900">
-            {children}
+            <div className="bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white">
+                <div className="flex">
 
+                    <Sidebar />
+                    <div className=" w-full text-slate-900">
+                        {children}
+
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    </>
+        </>
     )
 }
