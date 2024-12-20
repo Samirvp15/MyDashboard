33791cd0ce19b29094890b54a39e76c42a8b91f1
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-12-19 21:31:22 -0500

    add initCounterState as reducer for Counter

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useAppDispatch, useAppSelector } from "@/store"
-import { addOne, substractOne } from "@/store/counter/counterSlice"
+import { addOne, initCounterState, substractOne } from "@/store/counter/counterSlice"
+import { useEffect } from "react"
 
 
 
@@ -15,18 +16,23 @@ export function CartCounter({ value = 0 }: CartCounterProps) {
     const count = useAppSelector(state => state.counter.count)
     const dispatch = useAppDispatch()
 
+    useEffect(() => {
+        dispatch(initCounterState(value))
+    }, [dispatch, value])
+
+
     return (
         <>
             <span className=" text-9xl">{count}</span>
 
             <div className=" flex">
                 <button
-                    onClick={() => dispatch(addOne())}
+                    onClick={() => dispatch(substractOne())}
                     className=" mx-5 w-16 justify-center  flex items-center p-2 rounded bg-gray-500 text-white">
                     -1
                 </button>
                 <button
-                    onClick={() => dispatch(substractOne())}
+                    onClick={() => dispatch(addOne())}
                     className=" mx-5 w-16 justify-center flex items-center p-2 rounded bg-gray-500 text-white">
                     +1
                 </button>
